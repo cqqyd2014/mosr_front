00fac312c644593f5d83cd9bd1ed85ebf2707eeb
wangil <wangli2000_cn@126.com>
2020-01-31 12:19:01 +0800

    20200131

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,12 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { Hello } from "./components/Hello";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-
-if ((module as any).hot) {
-	
-	module.hot.accept(() => {
-		ReactDOM.render(
-			<Hello compiler="TypeScript" framework="React" />,
-			document.getElementById("mosr_index") as HTMLElement
-		)
-	})
-};
-
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("mosr_index") as HTMLElement
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
